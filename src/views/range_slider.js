093aa94890f606198c25e601836820956aac96b1
adorsk <adorsk>
2012-03-30 17:36:51 -0400

    added styling for left/right of slider

--- src/views/range_slider.js
+++ src/views/range_slider.js
@@ -32,6 +32,41 @@ function($, Backbone, _, ui, _s, template){
 				values: [ this.model.get('selection_min'), this.model.get('selection_max')],
 			});
 
+			// Add divs to the left and right of the slider.
+			$('.ui-slider-range', this.el).before('<div class="ui-slider-range-left" style="position: absolute;"></div>');
+			$('.ui-slider-range', this.el).after('<div class="ui-slider-range-right" style="position: absolute;"></div>');
+
+			// Set initial widths of the divs.
+			this.slider_el.slider("option", "values", [this.model.get('selection_min'), this.model.get('selection_max')]);
+
+			// Have divs track the slider.
+			var _this = this;
+			this.slider_el.on('slide', function(){
+				_this.updateRangeLeftRight();
+			});
+			
+		},
+
+		getSliderBounds: function(){
+			var slider_range = $('.ui-slider-range', this.el);
+			var range_pos = slider_range.position();
+			var range_width = slider_range.width();
+			var range_height = slider_range.height();
+			return {
+				top: range_pos.top, 
+				right: range_pos.left + range_width, 
+				bottom: range_pos.top + range_height, 
+				left: range_pos.left,
+				width: range_width,
+				height: range_height
+			};
+		},
+
+		updateRangeLeftRight: function(){
+			var slider_bounds = this.getSliderBounds();
+			$('.ui-slider-range-left', this.el).css('width', slider_bounds.left);
+			$('.ui-slider-range-right', this.el).css('width', $('.ui-slider', this.el).width() - slider_bounds.right);
+			$('.ui-slider-range-right', this.el).css('left', slider_bounds.right);
 		},
 
 		onSlideStop: function(){
