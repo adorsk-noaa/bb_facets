b0294c76645efd5180d61f62c9623d61df37913c
adorsk <adorsk>
2012-07-03 09:16:33 -0400

    added time slider facet

--- src/views/main.js
+++ src/views/main.js
@@ -4,9 +4,10 @@ define([
 	"./list_facet",
 	"./numeric_facet",
 	"./facet_collection",
-	"./range_slider"
+	"./range_slider",
+	"./time_slider_facet"
 ], 
-function(_, RadioSelectFacetView, ListFacetView, NumericFacetView, FacetCollectionView, RangeSliderView){
+function(_, RadioSelectFacetView, ListFacetView, NumericFacetView, FacetCollectionView, RangeSliderView, TimeSliderFacetView){
 
 	views = {
 		RadioSelectFacetView: RadioSelectFacetView,
@@ -14,6 +15,7 @@ function(_, RadioSelectFacetView, ListFacetView, NumericFacetView, FacetCollecti
 		NumericFacetView: NumericFacetView,
 		FacetCollectionView: FacetCollectionView,
 		RangeSliderView: RangeSliderView,
+        TimeSliderFacetView: TimeSliderFacetView
 	};
 
 	return views;
