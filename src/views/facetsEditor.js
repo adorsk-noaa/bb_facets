5e21ff6052369ab2d0462c8f87e6e756c4771eed
adorsk <adorsk>
2012-12-12 10:02:44 -0500

    partway through tabble refactor

--- src/views/facetsEditor.js
+++ src/views/facetsEditor.js
@@ -1,263 +1,205 @@
-define([
-	"jquery",
-	"backbone",
-	"underscore",
-	"_s",
-	"ui",
-	"Menus",
-	"Util",
-	"./facet_collection",
-	"./summaryBar",
-	"text!./templates/facetsEditor.html"
-		],
-function($, Backbone, _, _s, ui, Menus, Util, FacetCollectionView, SummaryBarView, template){
+define(
+  [
+    "jquery",
+    "backbone",
+    "underscore",
+    "_s",
+    "ui",
+    "Menus",
+    "tabble",
+    "Util",
+    "./facet_collection",
+    "./summaryBar",
+    "text!./templates/facetsEditor.html"
+],
+function($, Backbone, _, _s, ui, Menus, Tabble, Util, FacetCollectionView, SummaryBarView, template){
 
-	var FacetsEditorView = Backbone.View.extend({
-        events: {
-            'click .tab .title': 'toggleConfigurationEditor',
-        },
+  var FacetsEditorView = Backbone.View.extend({
+    initialize: function(){
+      $(this.el).addClass('facets-editor');
 
-		initialize: function(){
-            $(this.el).addClass('facets-editor');
-
-            // Initialize sub-collections if not provided.
-            _.each(['quantity_fields', 'facets', 'predefined_facets'], function(attr){
-                var collection = this.model.get(attr);
-                if (! collection){
-                    collection = new Backbone.Collection();
-                    this.model.set(attr, collection);
-                }
-            }, this);
-
-            // Initialize summary bar model if not provided.
-            var summaryBarModel = this.model.get('summary_bar');
-            if (! summaryBarModel){
-                this.model.set('summary_bar', new Backbone.Model());
-            }
-
-            // Initialize qfield selector model if not provided.
-            // @TODO: should probably encapsulate this as
-            // 'this.model.get('selected_quantity_field') later...
-            var qFieldSelectModel = this.model.get('qfield_select');
-            if (! qFieldSelectModel){
-                // Create choices.
-                var choices = this.formatQFieldChoices();
-                // Create model.
-                qFieldSelectModel = new Backbone.Model({
-                    choices: choices
-                });
-                this.model.set('qfield_select', qFieldSelectModel);
-            }
-
-            // Registry for sub-views.
-            this.subViews = {};
-
-            this.initialRender();
-
-            // Listen for ready events.
-            this.on('ready', this.onReady, this);
-
-            // Listen for resize events.
-            this.on('resize', this.resize, this);
-		},
-
-        initialRender: function(){
-            var html = _.template(template, {model: this.model});
-            $(this.el).html(html);
-
-            this.qFieldSelect = new Util.views.InfoSelectView({
-                el : $('.quantity-field-selector', this.el),
-                model: this.model.get('qfield_select')
-            });
-
-            // Render summary bar.
-            this.subViews.summaryBar = new SummaryBarView({
-                el: $('.summary-bar', this.el),
-                model: this.model.get('summary_bar')
-            });
-
-            // Render facet collection.
-            var collection_view_class = this.getFacetCollectionViewClass();
-            var view = new collection_view_class({
-                el: $('.facets', this.el),
-                model: this.model.get('facets')
-            });
-            this.subViews.facets = view;
-
-            // Render add facets menu.
-            this.renderAddFacetsMenu();
-
-            // Do initial resize.
-            this.resize();
-        },
-
-        // This is intended to be overriden to allow
-        // for formatting decoration.
-        formatter: function(s){
-            return s;
-        },
-
-        getFacetCollectionViewClass: function(){
-            return FacetCollectionView;
-        },
-
-        renderAddFacetsMenu: function(){
-
-            // Define a function that adds a facet to the facet collection,
-            // given a facet definition.
-            var _this = this;
-            var addFacet = function(facetModel){
-                _this.model.get('facets').add(facetModel);
-            };
-
-            // Format menu items from predefined facets.
-            var menuItems = [];
-            _.each(this.model.get('predefined_facets').models, function(facetDef){
-                var $content = $('<div>' + facetDef.get('facetDef').label + '</div>');
-                // Assign create facet function to content.
-                $content.on('click', function(){
-                    (function(def){
-                        // Create model from definition.
-                        var facetModel = _this.createFacetModelFromDef(def.get('facetDef'));
-                        addFacet(facetModel);
-                    })(facetDef);
-                });
-                var menuItem = {
-                    content: $content,
-                    id: facetDef.id
-                };
-
-                // Add menu item to list.
-                menuItems.push(menuItem);
-            }, this);
-
-            // Create menu with 'Add Facet..' menu item.
-            var menu = {
-                items: [{content: 'Add Facet', items: menuItems}]
-            };
-
-            // Create menu model.
-            var menuModel = new Backbone.Model({
-                menu: menu
-            });
-
-            // Create menu view.
-            var menuView = new Menus.views.TooltipMenuView({
-                el: $('.add-facet-button', this.el),
-                model: menuModel
-            });
-
-        },
-
-        formatQFieldChoices: function(){
-            var choices = [];
-            _.each(this.model.get('quantity_fields').models, function(model){
-                choices.push({
-                    value: model.id,
-                    label: this.formatter(model.get('label')),
-                    info: this.formatter(model.get('info'))
-                });
-            }, this);
-            return choices;
-        },
-
-        createFacetModelFromDef: function(facetDef){
-            var facetModel = new Backbone.Model(_.extend({
-            }, facetDef));
-
-            // Set id if none was given.
-            if (! facetModel.get('id')){
-                facetModel.set('id', facetModel.cid);
-            }
-
-            return facetModel;
-        },
-
-        toggleConfigurationEditor: function(){
-            var $tab = $('.tab', this.el);
-            var $editorCell = $('.configuration-editor-cell', this.el);
-            var $table = $('> .inner > .body > .table', this.el);
-
-            // Do nothing if currently changing.
-            if ($editorCell.hasClass('changing')){
-                return;
-            }
-
-
-            // Calculate how much to change dimension.
-            var delta = parseInt($editorCell.css('maxWidth'), 10) - parseInt($editorCell.css('minWidth'), 10);
-            // Determine whether to expand or contract.
-            var expanded = $editorCell.hasClass('expanded');
-            if (expanded){
-                delta = -1 * delta;
-            }
-
-
-            // Toggle button text
-            var button_text = ($('button.toggle', $tab).html() == '\u25B2') ? '\u25BC' : '\u25B2';
-            $('button.toggle', $tab).html(button_text);
-
-            // Execute animations and save deferreds.
-            var deferreds = [];
-
-            // Animate field container dimension.
-
-            $editorCell.addClass('changing');
-
-            var deferreds = [];
-
-            var cellDeferred = $editorCell.animate(
-                    {
-                        width: $editorCell.width() + delta
-                    },
-                    {
-                        complete: function(){
-                            $editorCell.removeClass('changing');
-
-                            if (! expanded){
-                                $editorCell.addClass('expanded')
-                            }
-                            else{
-                                $editorCell.removeClass('expanded');
-                            }
-                        }
-                    }
-                    ).promise();
-
-            deferreds.push(cellDeferred);
-
-            // Animate table dimension.
-            var tableDeferred = $table.animate(
-                    {
-                        width: $table.width() + delta
-                    }
-                    ).promise();
-            deferreds.push(tableDeferred);
-
-            // Return combined deferred.
-            return $.when.apply($, deferreds);
-        },
-
-        resize: function(){
-            var $table = $('.facets-editor-table', this.el);
-            Util.util.fillParent($table);
-            this.resizeVerticalTab($('.tab', this.el)); 
-        },
-
-        resizeVerticalTab: function($vt){
-            var $rc = $('.rotate-container', $vt);
-            $rc.css('width', $rc.parent().height());
-            $rc.css('height', $rc.parent().width());
-        },
-
-        onReady: function(){
-            _.each(this.subViews, function(subView){
-                subView.trigger('ready');
-            });
+      // Initialize sub-collections if not provided.
+      _.each(['quantity_fields', 'facets', 'predefined_facets'], function(attr){
+        var collection = this.model.get(attr);
+        if (! collection){
+          collection = new Backbone.Collection();
+          this.model.set(attr, collection);
         }
+      }, this);
 
-	});
+      // Initialize summary bar model if not provided.
+      var summaryBarModel = this.model.get('summary_bar');
+      if (! summaryBarModel){
+        this.model.set('summary_bar', new Backbone.Model());
+      }
 
-	return FacetsEditorView;
+      // Initialize qfield selector model if not provided.
+      // @TODO: should probably encapsulate this as
+      // 'this.model.get('selected_quantity_field') later...
+      var qFieldSelectModel = this.model.get('qfield_select');
+      if (! qFieldSelectModel){
+        // Create choices.
+        var choices = this.formatQFieldChoices();
+        // Create model.
+        qFieldSelectModel = new Backbone.Model({
+          choices: choices
+        });
+        this.model.set('qfield_select', qFieldSelectModel);
+      }
+
+      // Registry for sub-views.
+      this.subViews = {};
+
+      this.initialRender();
+
+      // Listen for ready events.
+      this.on('ready', this.onReady, this);
+
+      // Listen for resize events.
+      this.on('resize', this.resize, this);
+      this.on('resizeStop', this.resizeStop, this);
+    },
+
+    initialRender: function(){
+      $(this.el).html(_.template(template, {model: this.model}));
+      this.$table = $(this.el).find('> table').eq(0);
+      this.$table.tabble({
+        stretchTable: true
+      });
+
+      this.qFieldSelect = new Util.views.InfoSelectView({
+        el : $('.quantity-field-selector', this.el),
+        model: this.model.get('qfield_select')
+      });
+      this.subViews.qFieldSelect = this.qFieldSelect;
+
+      // Render summary bar.
+      this.subViews.summaryBar = new SummaryBarView({
+        el: $('.summary-bar', this.el),
+        model: this.model.get('summary_bar')
+      });
+
+      // Render facet collection.
+      var collection_view_class = this.getFacetCollectionViewClass();
+      var view = new collection_view_class({
+        el: $('.facets', this.el),
+        model: this.model.get('facets')
+      });
+      this.subViews.facets = view;
+
+      // Render add facets menu.
+      this.renderAddFacetsMenu();
+
+      // Do initial resize.
+      this.resize();
+      this.resizeStop();
+    },
+
+    // This is intended to be overriden to allow
+    // for formatting decoration.
+    formatter: function(s){
+      return s;
+    },
+
+    getFacetCollectionViewClass: function(){
+      return FacetCollectionView;
+    },
+
+    renderAddFacetsMenu: function(){
+
+      // Define a function that adds a facet to the facet collection,
+      // given a facet definition.
+      var _this = this;
+      var addFacet = function(facetModel){
+        _this.model.get('facets').add(facetModel);
+      };
+
+      // Format menu items from predefined facets.
+      var menuItems = [];
+      _.each(this.model.get('predefined_facets').models, function(facetDef){
+        var $content = $('<div>' + facetDef.get('facetDef').label + '</div>');
+        // Assign create facet function to content.
+        $content.on('click', function(){
+          (function(def){
+            // Create model from definition.
+            var facetModel = _this.createFacetModelFromDef(def.get('facetDef'));
+            addFacet(facetModel);
+          })(facetDef);
+        });
+        var menuItem = {
+          content: $content,
+          id: facetDef.id
+        };
+
+        // Add menu item to list.
+        menuItems.push(menuItem);
+      }, this);
+
+      // Create menu with 'Add Facet..' menu item.
+      var menu = {
+        items: [{content: 'Add Facet', items: menuItems}]
+      };
+
+      // Create menu model.
+      var menuModel = new Backbone.Model({
+        menu: menu
+      });
+
+      // Create menu view.
+      var menuView = new Menus.views.TooltipMenuView({
+        el: $('.add-facet-button', this.el),
+        model: menuModel
+      });
+
+    },
+
+    formatQFieldChoices: function(){
+      var choices = [];
+      _.each(this.model.get('quantity_fields').models, function(model){
+        choices.push({
+          value: model.id,
+          label: this.formatter(model.get('label')),
+          info: this.formatter(model.get('info'))
+        });
+      }, this);
+      return choices;
+    },
+
+    createFacetModelFromDef: function(facetDef){
+      var facetModel = new Backbone.Model(_.extend({
+      }, facetDef));
+
+      // Set id if none was given.
+      if (! facetModel.get('id')){
+        facetModel.set('id', facetModel.cid);
+      }
+
+      return facetModel;
+    },
+
+    resize: function(){
+      _.each(this.subViews, function(subView){
+        subView.trigger('resize');
+      });
+    },
+
+    resizeStop: function(){
+      this.$table.tabble('resize');
+      _.each(this.subViews, function(subView){
+        subView.trigger('resizeStop');
+      });
+    },
+
+    onReady: function(){
+      this.resize();
+      this.resizeStop();
+      _.each(this.subViews, function(subView){
+        subView.trigger('ready');
+      });
+    }
+
+  });
+
+  return FacetsEditorView;
 });
-		
+
--- src/views/summaryBar.js
+++ src/views/summaryBar.js
@@ -1,74 +1,79 @@
-define([
-	"jquery",
-	"backbone",
-	"underscore",
-	"_s",
-	"ui",
-	"Util",
-		],
+define(
+  [
+    "jquery",
+    "backbone",
+    "underscore",
+    "_s",
+    "ui",
+    "Util",
+],
 function($, Backbone, _, _s, ui, Util){
 
-	var SummaryBarView = Backbone.View.extend({
+  var SummaryBarView = Backbone.View.extend({
 
-        initialize: function(opts){
-            // Set default formatter if none given.
-            if (! opts.formatter){
-                this.formatter = _s.sprintf;
-            }
-    
-            this.initialRender();
+    initialize: function(opts){
+      // Set default formatter if none given.
+      if (! opts.formatter){
+        this.formatter = _s.sprintf;
+      }
 
-            // Trigger update when model data changes.
-            this.model.on('change:data', this.onDataChange, this);
+      this.initialRender();
 
-            // Listen for ready events.
-            this.on('ready', this.onReady, this);
-        },
+      // Trigger update when model data changes.
+      this.model.on('change:data', this.onDataChange, this);
 
-        initialRender: function(){
-            $(this.el).html('<div class="inner"><div class="text"><div><span class="field"></span>:<div class="selected"></div><div class="total"></div></div></div>');
-        },
+      // Listen for ready events.
+      this.on('ready', this.onReady, this);
 
-        onDataChange: function(){
-            var data = this.model.get('data');
+      // Listen for resize events.
+      this.on('resize', this.resize, this);
+      this.on('resizeStop', this.resizeStop, this);
+    },
 
-            // Do nothing if data is incomplete.
-            if (! data || data.selected == null || data.total == null){
-                return;
-            }
+    initialRender: function(){
+      $(this.el).html('<div class="inner"><div class="text"><div><span class="field"></span>:<div class="selected"></div><div class="total"></div></div></div>');
+    },
 
-            var format = '%s';
-            var qField = this.model.get('quantity_field');
-            if (qField){
-                format = qField.get('format');
-            }
+    onDataChange: function(){
+      var data = this.model.get('data');
 
-            var formatted_selected = this.formatter(format, data.selected);
-            var formatted_total = this.formatter(format, data.total);
-            var percentage;
-            if (data.total == 0 && data.selected == 0){
-                percentage = 100.0;
-            }
-            else{
-                percentage = 100.0 * data.selected/data.total;
-            }
+      // Do nothing if data is incomplete.
+      if (! data || data.selected == null || data.total == null){
+        return;
+      }
 
-            $(".text .field", this.el).html(_s.sprintf("'%s'", this.model.get('quantity_field').get('label')));
-            $(".text .selected", this.el).html(formatted_selected);
-            $(".text .total", this.el).html(_s.sprintf('(%.1f%% of %s total)', percentage, formatted_total));
+      var format = '%s';
+      var qField = this.model.get('quantity_field');
+      if (qField){
+        format = qField.get('format');
+      }
 
-            this.trigger('change:size');
+      var formatted_selected = this.formatter(format, data.selected);
+      var formatted_total = this.formatter(format, data.total);
+      var percentage;
+      if (data.total == 0 && data.selected == 0){
+        percentage = 100.0;
+      }
+      else{
+        percentage = 100.0 * data.selected/data.total;
+      }
 
-        },
+      $(".text .field", this.el).html(_s.sprintf("'%s'", this.model.get('quantity_field').get('label')));
+      $(".text .selected", this.el).html(formatted_selected);
+      $(".text .total", this.el).html(_s.sprintf('(%.1f%% of %s total)', percentage, formatted_total));
 
-        // Update display on ready.
-        onReady: function(){
-            this.onDataChange();
-        }
+      this.trigger('change:size');
+
+    },
+
+    // Update display on ready.
+    onReady: function(){
+      this.onDataChange();
+    }
 
 
-	});
+  });
 
-	return SummaryBarView;
+  return SummaryBarView;
 });
-		
+
