383b2bf3a8c0352f61bae6d0feb4f2e0ba38bfb3
adorsk <adorsk>
2013-01-17 19:31:28 -0500

    took out summary bar from facetsEditor

--- src/views/facetsEditor.js
+++ src/views/facetsEditor.js
@@ -9,10 +9,9 @@ define(
     "tabble",
     "Util",
     "./facet_collection",
-    "./summaryBar",
     "text!./templates/facetsEditor.html"
 ],
-function($, Backbone, _, _s, ui, Menus, Tabble, Util, FacetCollectionView, SummaryBarView, template){
+function($, Backbone, _, _s, ui, Menus, Tabble, Util, FacetCollectionView, template){
 
   var FacetsEditorView = Backbone.View.extend({
     initialize: function(){
@@ -27,28 +26,6 @@ function($, Backbone, _, _s, ui, Menus, Tabble, Util, FacetCollectionView, Summa
         }
       }, this);
 
-      // Initialize summary bar model if not provided.
-      var summaryBarModel = this.model.get('summary_bar');
-      if (! summaryBarModel){
-        this.model.set('summary_bar', new Backbone.Model());
-      }
-
-      // Initialize qfield selector model if not provided.
-      // @TODO: should probably encapsulate this as
-      // 'this.model.get('selected_quantity_field') later...
-      /*
-      var qFieldSelectModel = this.model.get('qfield_select');
-      if (! qFieldSelectModel){
-        // Create choices.
-        var choices = this.formatQFieldChoices();
-        // Create model.
-        qFieldSelectModel = new Backbone.Model({
-          choices: choices
-        });
-        this.model.set('qfield_select', qFieldSelectModel);
-      }
-      */
-
       // Registry for sub-views.
       this.subViews = {};
 
@@ -56,9 +33,6 @@ function($, Backbone, _, _s, ui, Menus, Tabble, Util, FacetCollectionView, Summa
 
       // Listen for ready events.
       this.on('ready', this.onReady, this);
-
-      // Listen for qfield change events.
-      this.model.on('change:selected_qfield', this.onSelectedQFieldChange, this);
 
       // Listen for resize events.
       this.on('resize', this.resize, this);
@@ -70,14 +44,6 @@ function($, Backbone, _, _s, ui, Menus, Tabble, Util, FacetCollectionView, Summa
       this.$table = $(this.el).find('> table').eq(0);
       this.$table.tabble({
         stretchTable: true
-      });
-
-      this.renderSelectParameterMenu();
-
-      // Render summary bar.
-      this.subViews.summaryBar = new SummaryBarView({
-        el: $('.summary-bar', this.el),
-        model: this.model.get('summary_bar')
       });
 
       // Render facet collection.
@@ -104,50 +70,6 @@ function($, Backbone, _, _s, ui, Menus, Tabble, Util, FacetCollectionView, Summa
 
     getFacetCollectionViewClass: function(){
       return FacetCollectionView;
-    },
-
-    renderSelectParameterMenu: function(){
-
-      // Define a function that adds a facet to the facet collection,
-      // given a facet definition.
-      var _this = this;
-
-      // Format menu items from quantity fields.
-      var menuItems = [];
-
-      _.each(this.model.get('quantity_fields').models, function(qfieldModel){
-        var $content = $('<div>' + qfieldModel.get('label') + '</div>');
-        // Assign select parameter function to content.
-        $content.on('click', function(){
-          (function(qfieldModel){
-            _this.model.set('selected_qfield', qfieldModel);
-          })(qfieldModel);
-        });
-        var menuItem = {
-          content: $content,
-          id: qfieldModel.id
-        };
-
-        // Add menu item to list.
-        menuItems.push(menuItem);
-      }, this);
-
-      // Create menu with 'Select Parameter..' menu item.
-      var menu = {
-        items: [{content: 'Select Parameter...', items: menuItems}]
-      };
-
-      // Create menu model.
-      var menuModel = new Backbone.Model({
-        menu: menu
-      });
-
-      // Create menu view.
-      var menuView = new Menus.views.TooltipMenuView({
-        el: $('.select-parameter-button', this.el),
-        model: menuModel
-      });
-
     },
 
     renderAddFacetsMenu: function(){
@@ -208,11 +130,6 @@ function($, Backbone, _, _s, ui, Menus, Tabble, Util, FacetCollectionView, Summa
       }
 
       return facetModel;
-    },
-
-    onSelectedQFieldChange: function(){
-      var qfieldModel = this.model.get('selected_qfield');
-      $('.selected-parameter-title', this.el).html(qfieldModel.get('label'));
     },
 
     resize: function(){
